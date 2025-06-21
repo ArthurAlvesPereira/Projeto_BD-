f4c553b4c76ed8c96e01b3405edb41356a604f47
Arthur <arthurap2001@gmail.com>
2025-06-21 13:01:56 -0300

    feat: Spring inicializado e conexão com banco feita

---
+++ src/main/java/br/com/arthur/sistema_avaliacoes/SistemaAvaliacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.arthur.sistema_avaliacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAvaliacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAvaliacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arthur/sistema_avaliacoes/SistemaAvaliacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arthur.sistema_avaliacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAvaliacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
